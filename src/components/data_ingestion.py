d0cbf334ab90e45756809d5ee9db3214686e9368
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2026-05-31 00:44:07 +0530

    feat: add data ingestion configuration

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+import os
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+
+    raw_data_path: str = os.path.join("artifacts", "raw.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
